59eae2b5b52641d25af3a88269488bfde1eb9aa5
Xander Deseyn <xander@devizer.io>
2020-11-03 14:05:30 +0100

    Add liquidity events

--- contracts/LenderPool.sol
+++ contracts/LenderPool.sol
@@ -9,6 +9,9 @@ contract LenderPool {
     mapping (address => uint) _balances;
     address _owner;
 
+    event LiquidityAdded(uint amount, address addr);
+    event LiquidityRemoved(uint amount, address addr);
+
     constructor() {
         console.log("Deploying LenderPool");
         console.log("Owner:", msg.sender);
@@ -16,13 +19,18 @@ contract LenderPool {
 
     receive() external payable {
         _balances[msg.sender] += msg.value;
+        emit LiquidityAdded(msg.value, msg.sender);
     }
+
+
 
     function withdraw() public returns (uint amount) {
         require(amount <= _balances[msg.sender] + _rewards[msg.sender]);
         _balances[msg.sender] -= amount;
         (bool success,) = msg.sender.call{ value: amount }("");
         require(success);
+
+        emit LiquidityRemoved(amount, msg.sender);
     }
 
      function balanceOf(address addr) public view returns (uint balance) {
