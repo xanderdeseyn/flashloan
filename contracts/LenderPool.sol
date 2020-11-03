93a6d0f7e3cd6d4b14dcde20f1fad011ac8eed73
Xander Deseyn <xander@devizer.io>
2020-11-03 21:41:52 +0100

    More elaborate deposit and withdraw test

--- contracts/LenderPool.sol
+++ contracts/LenderPool.sol
@@ -6,12 +6,14 @@ import "hardhat/console.sol";
 /// @title The LenderPool contract which allows borrowers to make flashloans
 /// @author Xander Deseyn
 contract LenderPool {
-
     uint constant LOAN_FEE_BASIS_POINTS = 5;
+    uint constant DEPOSIT_GRANULARITY = 10 ** 9;
 
-    mapping (address => uint) _rewards;
-    mapping (address => uint) _balances;
+    mapping (address => uint) _rewardsGeneratedAtTimeOfDeposit;
+    mapping (address => uint) _deposits;
     address _owner;
+    uint totalDeposited;
+    uint totalRewardsGenerated;
 
     event LiquidityAdded(uint amount, address addr);
     event LiquidityRemoved(uint amount, address addr);
@@ -25,37 +27,49 @@ contract LenderPool {
     /// @dev This could also be moved to a function, but I decided this is the easiest way for users to provide liquidity. In addition, this prevents ETH sent to the contract from being lost.
     /// @notice Sending ETH to this contract automatically makes you a liquidity provider
     receive() external payable {
-        _balances[msg.sender] += msg.value;
+        require(_deposits[msg.sender] == 0);
+        _deposits[msg.sender] = msg.value;
+        totalDeposited += msg.value;
+        _rewardsGeneratedAtTimeOfDeposit[msg.sender] = totalRewardsGenerated;
         emit LiquidityAdded(msg.value, msg.sender);
     }
 
-    /// @notice A user can withdraw his provided liquidity + any liquidity rewards he gained
-    /// @param amount The amount of wei to withdraw
-    function withdraw(uint amount) external {
-        require(amount <= _balances[msg.sender] + _rewards[msg.sender]);
-        _balances[msg.sender] -= amount;
-        (bool success,) = msg.sender.call{ value: amount }("");
+    /// @notice A user can only withdraw all his provided liquidity + all liquidity rewards he gained. This is for performance reasons http://batog.info/papers/scalable-reward-distribution.pdf
+    function withdraw() external {
+        uint deposit = _deposits[msg.sender];
+        require(deposit > 0);
+        _deposits[msg.sender] = 0;
+        
+        uint reward = (totalRewardsGenerated - _rewardsGeneratedAtTimeOfDeposit[msg.sender]) * deposit;
+
+        (bool success,) = msg.sender.call{ value: deposit + reward }("");
         require(success);
 
-        emit LiquidityRemoved(amount, msg.sender);
+        emit LiquidityRemoved(deposit + reward, msg.sender);
     }
 
-    /// @param addr The address whose balance to check
-    /// @return balance The balance for addr
-    function balanceOf(address addr) public view returns (uint balance) {
-        return _balances[addr];
+    /// @param addr The address whose deposit to check
+    /// @return deposit The deposit for addr
+    function depositOf(address addr) public view returns (uint deposit) {
+        return _deposits[addr];
     }
 
     /// @dev The borrower has to repay at least amountLended * (1 + LOAN_FEE_BASIS_POINTS / 10000) before his function returns control to this contract
     /// @param amount The amount to lend in wei
     function flashloan(uint amount) external {
         uint initialLiquidity = address(this).balance;
-        require(amount <= address(this).balance);
+        require(amount <= initialLiquidity);
 
         (bool success,) = msg.sender.call{ value: amount }("");
         require(success);
 
         uint newLiquidity = address(this).balance;
         require(newLiquidity >= (initialLiquidity + initialLiquidity * LOAN_FEE_BASIS_POINTS / 10000));
+        distributeRewards(newLiquidity - initialLiquidity);
+    }
+
+    function distributeRewards(uint amount) internal {
+        require(amount > 0);
+        totalRewardsGenerated = totalRewardsGenerated + amount / totalDeposited;
     }
 }
--- test/flashloan.test.ts
+++ test/flashloan.test.ts
@@ -4,13 +4,22 @@ import "@nomiclabs/hardhat-waffle";
 import { LenderPool } from "../types/LenderPool";
 import { Borrower } from "../types/Borrower";
 import { ethers as E } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+
+const oneETH = parseEther("1");
+const twoETH = parseEther("2");
+const threeETH = parseEther("3");
 
 describe("Flashloan", function () {
   let provider: E.providers.JsonRpcProvider;
   let signer1: E.providers.JsonRpcSigner;
   let signer2: E.providers.JsonRpcSigner;
+  let signer3: E.providers.JsonRpcSigner;
+  let signer4: E.providers.JsonRpcSigner;
   let signer1Address: string;
   let signer2Address: string;
+  let signer3Address: string;
+  let signer4Address: string;
   let lenderPool: LenderPool;
   let borrower: Borrower;
 
@@ -18,9 +27,13 @@ describe("Flashloan", function () {
     provider = new ethers.providers.JsonRpcProvider();
     signer1 = provider.getSigner(0);
     signer2 = provider.getSigner(1);
+    signer3 = provider.getSigner(2);
+    signer4 = provider.getSigner(3);
 
     signer1Address = await signer1.getAddress();
     signer2Address = await signer2.getAddress();
+    signer3Address = await signer3.getAddress();
+    signer4Address = await signer4.getAddress();
 
     const LenderPoolFactory = await ethers.getContractFactory(
       "LenderPool",
@@ -41,12 +54,43 @@ describe("Flashloan", function () {
   it("Should be able to lend and withdraw liquidity", async function () {
     await signer1.sendTransaction({
       to: lenderPool.address,
-      value: 100,
+      value: oneETH,
     });
-    expect(await lenderPool.balanceOf(signer1Address)).to.equal(100);
-    await lenderPool.withdraw(50);
-    expect(await lenderPool.balanceOf(signer1Address)).to.equal(50);
-    await lenderPool.withdraw(50);
-    expect(await lenderPool.balanceOf(signer1Address)).to.equal(0);
+    await signer2.sendTransaction({
+      to: lenderPool.address,
+      value: twoETH,
+    });
+    await signer3.sendTransaction({
+      to: lenderPool.address,
+      value: threeETH,
+    });
+
+    expect(await lenderPool.depositOf(signer1Address)).to.equal(oneETH);
+    await lenderPool.withdraw();
+    expect(await lenderPool.depositOf(signer1Address)).to.equal(0);
+
+    expect(await lenderPool.depositOf(signer2Address)).to.equal(twoETH);
+    const lenderPool2 = lenderPool.connect(signer2);
+    await lenderPool2.withdraw();
+    expect(await lenderPool.depositOf(signer2Address)).to.equal(0);
+
+    expect(await lenderPool.depositOf(signer3Address)).to.equal(threeETH);
+    const lenderPool3 = lenderPool.connect(signer3);
+    await lenderPool3.withdraw();
+    expect(await lenderPool.depositOf(signer3Address)).to.equal(0);
   });
+
+  // it("Should be able to lend and withdraw liquidity", async function () {
+  //   await signer1.sendTransaction({
+  //     to: lenderPool.address,
+  //     value: oneETH,
+  //   });
+
+  //   await signer1.sendTransaction({
+  //     to: lenderPool.address,
+  //     value: oneETH,
+  //   });
+
+  //   lenderPool.connect(signer3);
+  // });
 });
